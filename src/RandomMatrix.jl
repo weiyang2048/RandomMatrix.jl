c96ed52de14d39043b1fbd31be05f4420f1ba694
weiyang607 <weiyang607@gmail.com>
2021-04-13 19:28:39 -0400

    minor fix

--- src/RandomMatrix.jl
+++ src/RandomMatrix.jl
@@ -44,18 +44,18 @@ S = Norm2SamplingMatrix(rand(2,3),rand(3,2),k=2)
  1.15342  0.0
 ```
 """
-function Norm2SamplingMatrix(A::Matrix,B=Matrix(A')::Matrix; k::Int)
+function Norm2SamplingMatrix(A::Matrix,B=Matrix(A')::Matrix; k=floor(Int,Size(A)[2]/2)::Int)
     n, m = size(A)
     if size(B)[1] != m
         return "The matrices can not be multiplied "
     else
         Lens = [norm(A[:,i])*norm(B[i,:]) for i = 1:m ]
         Total = sum(Lens)
-        temp = zeros(m,k)
+        S = zeros(m,k)
         for (i,j) in enumerate([sample([1:m...], Weights(Lens) ) for i = 1 : k])
-            temp[j,i] = 1/sqrt(Lens[j]/Total*k) # normalizing so that E[SS']=I
+            S[j,i] = 1/sqrt(Lens[j]/Total*k) # normalizing so that E[SS']=I
         end
-        return temp
+        return S
     end
 end
 
