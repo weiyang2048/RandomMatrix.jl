a871691fb0023598868682a2fc43c97dfb171f98
weiyang607 <weiyang607@gmail.com>
2021-04-13 01:18:25 -0400

    Added random orthogonal, unitary

--- src/RandomMatrix.jl
+++ src/RandomMatrix.jl
@@ -1,5 +1,18 @@
 module RandomMatrix
 
-greet() = print("Hello World!")
+using LinearAlgebra
+
+export RandomUnitaryMatrix, RandomOrthogonalMatrix
+
+function RandomUnitaryMatrix(n::Int)
+    A = randn(ComplexF64,n,n)
+    Q = convert(Matrix,qr(A).Q)
+    return Q
+end
+function RandomOrthogonalMatrix(n::Int)
+    A = randn(ComplexF64,n,n)
+    Q = convert(Matrix,qr(A).Q)
+    return Q
+end
 
 end # module
