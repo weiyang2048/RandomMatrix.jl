79140b5b89518ddc6a1e4301bd0ea99a778e8815
weiyang607 <weiyang607@gmail.com>
2021-04-13 14:42:23 -0400

    added some documentations

--- src/RandomMatrix.jl
+++ src/RandomMatrix.jl
@@ -4,6 +4,15 @@ using LinearAlgebra, StatsBase
 
 export RandomUnitaryMatrix, RandomOrthogonalMatrix 
 
+"""
+```julia
+RandomUnitaryMatrix(3)
+3×3 Matrix{ComplexF64}:
+ -0.173896+0.299143im  -0.173916+0.627558im  -0.666748-0.107907im
+  0.338596+0.832271im  0.0660322-0.146868im    0.18828-0.362351im
+ -0.202431-0.17881im    0.724265-0.159481im  -0.277831-0.547599im
+```
+"""
 function RandomUnitaryMatrix(n::Int)
     A = randn(ComplexF64,n,n)
     Q = convert(Matrix,qr(A).Q)
