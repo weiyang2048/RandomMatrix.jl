f44af3aef799d488cf896bc66aa65e76ad35e691
weiyang607 <weiyang607@gmail.com>
2021-04-14 18:39:04 -0400

    created Unitary.jl

--- README.md
+++ README.md
@@ -10,27 +10,13 @@ Hoping to add-in enough functionalities and publish in Aug 2021.
 ## Random Matrix Theory
 
 ### Random Matrix Models
-#### Random Unitary / Orthogonal
-- To generate a 3 by 3 random Unitary matrix run 
+- To generate a 3 by 3 **random Unitary matrix** run 
     - `RandomUnitaryMatrix(3)`  or equivalently
     - `rand(Haar(2,3))`
-- To generate a 3 by 3 random Orthogonal matrix run 
+- To generate a 3 by 3 **random Orthogonal matrix** run 
     - `RandomOrthogonalMatrix(3)` or equivalently
     - `rand(Haar(1,3))`
-```
 
 ## Randomized Linear Algebra
-```julia
-RandomNormSamplingMatrix(A::Matrix, B=Matrix(A')::Matrix; k=0::Int)
-```
-Generate a random norm-squared sampling random Matrix `S`, ``E(SS')=I``.
-ASS'B is typically used to approximate AB.
-
-```julia
-S = RandomNormSamplingMatrix(rand(2,3),rand(3,2),k=2)
-
-3×2 Matrix{Float64}:
- 0.0      0.0
- 0.0      1.2439
- 1.15342  0.0
-```
+- If $A\in R^{n,m}$ and $B\in R^{m,w}$, run `RandomSamplingMatrix(A,B,k=2)` will generate a random  sampling matrix $\mathcal{S}\in R^{m,k}$. Note that $\mathbf{E}(\mathcal{S}\mathcal{S}')=I$ and 
+ that $\mathbf{E}(A\mathcal{S}\mathcal{S}'B)=AB$.  For defefinition, check the code or look for definition at the end of section 2.2 in https://arxiv.org/pdf/1608.04481.pdf.
--- src/RandomMatrix.jl
+++ src/RandomMatrix.jl
@@ -2,50 +2,22 @@ module RandomMatrix
 
 using LinearAlgebra, StatsBase, Distributions
 
-export RandomUnitaryMatrix, RandomOrthogonalMatrix , Haar
+include("Unitary.jl")
 
-"""
-```julia
-RandomUnitaryMatrix(3)
-3×3 Matrix{ComplexF64}:
- -0.173896+0.299143im  -0.173916+0.627558im  -0.666748-0.107907im
-  0.338596+0.832271im  0.0660322-0.146868im    0.18828-0.362351im
- -0.202431-0.17881im    0.724265-0.159481im  -0.277831-0.547599im
-```
-"""
-function RandomUnitaryMatrix(n::Int)
-    A = randn(ComplexF64,n,n)
-    Q = convert(Matrix,qr(A).Q)
-    return Q
-end
-function RandomOrthogonalMatrix(n::Int)
-    A = randn(n,n)
-    Q = convert(Matrix,qr(A).Q)
-    return Q
-end
-
-struct Haar <: ContinuousMatrixDistribution
-    beta::Int
-    n::Int
-    Haar(beta,n) = beta in (1,2) ? new(beta,n) : error("Only take beta = 1 for orthogonal, beta = 2 for unitary")
-end
-Base.size(d::Haar) = d.n
-Base.rand(d::Haar) = d.beta == 1 ? RandomOrthogonalMatrix(d.n) : RandomUnitaryMatrix(d.n)
-
-export RandomNormSamplingMatrix
+export RandomSamplingMatrix
 
 
 """
 ```julia
-RandomNormSamplingMatrix(A::Matrix, B=Matrix(A')::Matrix; k=0::Int)
+RandomSamplingMatrix(A::Matrix, B=I::Matrix; k=0::Int)
 ```
-Generate a random norm-squared sampling random Matrix `S`, E(SS')=I.
+Generate a random sampling random Matrix `S`, E(SS')=I.
 ASS'B is typically used to approximate AB.
 
 
 # Examples
 ```julia
-S = RandomNormSamplingMatrix(rand(2,3),rand(3,2),k=2)
+S = RandomSamplingMatrix(rand(2,3),rand(3,2),k=2)
 
 3×2 Matrix{Float64}:
  0.0      0.0
@@ -53,7 +25,7 @@ S = RandomNormSamplingMatrix(rand(2,3),rand(3,2),k=2)
  1.15342  0.0
 ```
 """
-function RandomNormSamplingMatrix(A::Matrix,B=Matrix(A')::Matrix; k=0::Int)
+function RandomSamplingMatrix(A::Matrix,B=I::Matrix; k=0::Int)
     if k <= 0 
         k = floor(Int,size(A)[2]/2)
         print("k was <= $(0) or not provided, it is now set to $(k) by default")
