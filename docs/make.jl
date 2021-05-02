c73f2e408ee22c3f9a90e6439778902c1b2c9af9
weiyang2048 <weiyang2048@gmail.com>
2021-05-02 14:59:28 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, RandomMatrix
 
-makedocs(  modules = [RandomMatrix],
+makedocs(modules = [RandomMatrix],
       sitename="RandomMatrix.jl")
 
 deploydocs(repo = "github.com/weiyang2048/RandomMatrix.jl.git",devbranch = "main")
