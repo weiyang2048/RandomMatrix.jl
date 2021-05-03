f6025c3fa6dacf1506f445397ac866fc962e0860
weiyang2048 <weiyang2048@gmail.com>
2021-05-03 14:13:13 -0400

    Update StructuredMatrices.jl

--- src/StructuredMatrices.jl
+++ src/StructuredMatrices.jl
@@ -2,22 +2,18 @@ export randToeplitz
 
 """
 ```julia
-randToeplitz(d::D, n::Int;  norm = false::Bool, hermitian=true::Bool, complex =false::Bool)  where D<:Any
+randToeplitz(d::D, n::Int;  norm = false::Bool, hermitian=true::Bool)  where D<:Any
 ```
 - The limiting spectral distribution of random Toeplitz matrices is still an open problem
 """
-function  randToeplitz(d::D, n::Int;  norm = false::Bool, hermitian=true::Bool, complex =false::Bool)  where D<:Any
+function  randToeplitz(d::D, n::Int;  norm = false::Bool, hermitian=true::Bool)  where D<:Any
     if hermitian
       t = rand(d,n)
       t = [t[n:-1:2]'..., t...]
     else
       t = rand(d,2n-1)
     end
-    if complex
-        M = zeros(ComplexF64,n,n)
-    else
-        M =zeros(n,n)
-    end
+    M = zeros(eltype(d),n,n)
     for i in 1:n,j in 1:n
         M[i,j]=t[i-j+n]
     end
