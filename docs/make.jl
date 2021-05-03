dffb10443b183d2d2f215726895209b68e3df37f
weiyang2048 <weiyang2048@gmail.com>
2021-05-03 00:30:35 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, RandomMatrix
 
-const PAGES = any["Home" =>"index.md",
+const PAGES = Any["Home" =>"index.md",
       "Random Matrix Models" =>[
             "IID Matrices.md"=>"IID Matrices.md",
             "Others" =>"Others.md"
@@ -9,6 +9,9 @@ const PAGES = any["Home" =>"index.md",
 ]
 
 makedocs(modules = [RandomMatrix],
-      sitename="RandomMatrix.jl")
+      sitename="RandomMatrix.jl",
+      authors = "Wei Yang",
+      pages = PAGES
+      )
 
 deploydocs(repo = "github.com/weiyang2048/RandomMatrix.jl.git",devbranch = "main")
