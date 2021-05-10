c208d29fc0fc7f2e3ee24e21c28e11db71fc949d
weiyang2048 <weiyang2048@gmail.com>
2021-05-10 01:01:02 -0400

    corrected the definition of GOE GUE

--- src/HermitianMatrix.jl
+++ src/HermitianMatrix.jl
@@ -19,28 +19,28 @@ Generates a 2 by 2 random Hermitian matrix with off-diagonal entries from the St
 ```julia
 randHermitian(2)
 
-2×2 LinearAlgebra.Hermitian{ComplexF64, Matrix{ComplexF64}}:
- -0.385798+0.0im       0.789436-0.671441im
-  0.789436+0.671441im  -2.00856+0.0im
+2×2 Hermitian{ComplexF64, Matrix{ComplexF64}}:
+  0.382095+0.0im        -0.708469-0.0636734im
+ -0.708469+0.0636734im   0.336952+0.0im
 ```
-Generate a 3 by 3 Hermitian matrix, with off-diagonal entries `Circular(1)` and diagonal entries uniformly `-1` or `1`.
+Generate a 3 by 3 Hermitian matrix, with off-diagonal entries [`Circular`](@ref)(1) and diagonal entries uniformly `-1` or `1`.
 ```julia
 randHermitian(Circular(1),3,diag = (-1,1))
 
-3×3 LinearAlgebra.Hermitian{ComplexF64, Matrix{ComplexF64}}:
-     -1.0+0.0im        0.785839+0.0137306im  1.61019-0.207893im
- 0.785839-0.0137306im       1.0+0.0im        1.30573+0.723762im
-  1.61019+0.207893im    1.30573-0.723762im       1.0+0.0im
+3×3 Hermitian{ComplexF64, Matrix{ComplexF64}}:
+     1.0+0.0im       1.56259-0.676099im  1.39468-0.295073im
+ 1.56259+0.676099im     -1.0+0.0im       1.53369+0.296583im
+ 1.39468+0.295073im  1.53369-0.296583im     -1.0+0.0im
 ```
 Generate a random 2 by 2 Symmetric Matrix with entries  `Poisson(2)` rvs. 
 This is also be done with `randSymmetric(Poisson(2),3)`
 ```julia
 randHermitian(Poisson(2),3)
 
-3×3 LinearAlgebra.Hermitian{Int64, Matrix{Int64}}:
- 0  0  6
- 0  3  1
- 6  1  4
+3×3 Hermitian{Int64, Matrix{Int64}}:
+ 3  1  0
+ 1  1  2
+ 0  2  1
 ```
 Entries uniformly from ``\\{1,2,3,...,10\\}``
 ```julia
@@ -77,7 +77,7 @@ function randHermitian(n::Int; norm = false::Bool)
     return randHermitian(ComplexNormal(), n, diag=Normal(), norm = norm)
 end
 
-
+##########################################################
 """
 ```julia
 randSymmetric(d, n; Diag, norm) 
@@ -117,13 +117,21 @@ function randSymmetric(n::Int; norm = false::Bool)
     return randSymmetric(Normal(), n, norm = norm)
 end
 
+#########################################################
+
 """
 ```julia
 GOE <: ContinuousMatrixDistribution
 GOE(n)
+
+rand(M::GOE, norm::bool) 
+- `norm` : default `false`,  if `norm` set to `true`, then the matrix will be normlaized with ``\\operatorname{min}(n,m)^{-1/2}``.  
 ```
 
 - `n` : dimension
+- The **Gaussian Orthogonal Ensemble** (`GOE`) is an ensemble of random ``n \\times n`` Symmetric matrices 
+    ``M_{n}`` in which the upper-triangular entries are iid with distribution ``N(0,1)_{\\mathbf{R}}``, 
+    and the diagonal entries are iid with distribution ``N(0,2)_{\\mathbf{R}}``, and independent of the upper-triangular ones
 
 # Examples
 
@@ -132,25 +140,40 @@ Generate a 3 by 3 random matrix from GOE(3)
 rand(GOE(3))
 
 3×3 Symmetric{Float64, Matrix{Float64}}:
-  0.216246   0.148277  -0.71106
-  0.148277  -0.341516  -0.204024
- -0.71106   -0.204024  -0.338064
-
+ -1.62391   -0.451433    0.863883
+ -0.451433   0.0271799  -0.524854
+  0.863883  -0.524854   -0.00930624
 ```
+
+```julia
+rand(GOE(3),norm=true)
+
+3×3 Symmetric{Float64, Matrix{Float64}}:
+  0.302141   0.152634   -0.711679
+  0.152634  -0.0629327   0.103075
+ -0.711679   0.103075    1.51861
+```
+
 """
 struct GOE <: ContinuousMatrixDistribution
     n::Int
 end
 
-rand(rng::AbstractRNG, M::GOE) = randSymmetric(Normal(),M.n,diag=Normal(0,sqrt(2)),norm=true)
+rand( M::GOE; norm = false::Bool) = randSymmetric(Normal(),M.n,diag=Normal(0,sqrt(2)),norm=norm)
 
 """
 ```julia
 GUE <: ContinuousMatrixDistribution
 GUE(n)
+
+rand(M::GUE, norm::bool) 
+- `norm` : default `false`,  if `norm` set to `true`, then the matrix will be normlaized with ``\\operatorname{min}(n,m)^{-1/2}``.  
 ```
 
 - `n` : dimension
+- The **Gaussian Unitary Ensemble** (`GUE`) is an ensemble of random ``n \\times n`` Hermitian matrices 
+    ``M_{n}`` in which the upper-triangular entries are iid with distribution ``N(0,1)_{\\mathbf{C}}``, 
+    and the diagonal entries are iid with distribution ``N(0,1)_{\\mathbf{R}}``, and independent of the upper-triangular ones
 # Examples
 
 Generate a 3 by 3 random matrix from GUE(3)
@@ -158,13 +181,20 @@ Generate a 3 by 3 random matrix from GUE(3)
 rand(GUE(3))
 
 3×3 Hermitian{ComplexF64, Matrix{ComplexF64}}:
- -0.0127056+0.0im          -0.200015+0.00787052im  0.333224-0.157845im
-  -0.200015-0.00787052im  0.00753986+0.0im         -0.37598-0.202447im
-   0.333224+0.157845im      -0.37598+0.202447im    0.298683+0.0im
+ -0.883413+0.0im         1.09872+0.874884im     -0.1985-1.04778im
+   1.09872-0.874884im    1.55483+0.0im        -0.488532+1.18694im
+   -0.1985+1.04778im   -0.488532-1.18694im   -0.0823873+0.0im
+```
+
+```julia
+rand(GUE(2),norm=true)
+2×2 Hermitian{ComplexF64, Matrix{ComplexF64}}:
+ -0.457089+0.0im       -0.672713-0.102234im
+ -0.672713+0.102234im   0.380126+0.0im
 ```
 """
 struct GUE <: ContinuousMatrixDistribution
     n::Int
 end
 
-rand(rng::AbstractRNG, M::GUE) = randHermitian(ComplexNormal(),M.n,diag=Normal(),norm=true)
+rand( M::GUE; norm=false::Bool) = randHermitian(ComplexNormal(),M.n,diag=Normal(),norm=norm)
