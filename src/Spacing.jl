ac93e83e6662b0bc7fa95255dac5cb7b01faeca9
weiyang2048 <weiyang2048@gmail.com>
2021-05-16 17:54:07 -0400

    added unfoldedeigvals, and updated unfoldedspacings

--- src/Spacing.jl
+++ src/Spacing.jl
@@ -1,4 +1,4 @@
-export wignersurmise, unfoldedSpacings
+export wignersurmise,unfoldedeigvals ,unfoldedSpacings
 
 function wignersurmise(x::Real; beta = 1 :: Int)  
     if beta == 1
@@ -8,19 +8,14 @@ function wignersurmise(x::Real; beta = 1 :: Int)
     end
 end
 
-function unfoldedSpacings(E::Array,deg=3::Int; goe = false::Bool)
+function unfoldedeigvals(E::Array; deg=3::Int, goe = false::Bool)
     if goe
-        f(e) = begin
-            temp = 4-e^2
-            if temp>=0
-                length(E)* ( 1/2 + e/(pi*4)*sqrt(temp)+1/pi*atan(e/sqrt(temp)) )  
-            end      
-        end
-        return diff(filter(x->x!==nothing,f.(E)))
+        return length(E)*cdf(Semicircle(2),E)
     end
-    p(x) =polyfit(E[1:end-1],diff(E),deg)(x)
-        
-    D = p.(E[1:end-1])
-    diff(E)./D 
-end
+    
+    return polyfit(E,(x->sum(E.<x)).(E),deg).(E)
 
+end
+function unfoldedSpacings(E::Array;deg=3::Int, goe = false::Bool)
+    return diff(unfoldedeigvals(E,deg=deg,goe=goe))
+end
